b22db416cb70d259e11862249054051937f98a33
Ayushi40804 <ayushi40804@gmail.com>
2024-10-07 11:52:46 +0530

    module import error

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/gdamore/tcell/v2 v2.7.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/rivo/tview v0.0.0-20240921122403-a64fc48d7654 // indirect
+	github.com/rivo/tview v0.0.0-20240921122403-a64fc48d7654
 	github.com/rivo/uniseg v0.4.7 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/term v0.17.0 // indirect
--- internal/ui/tui.go
+++ internal/ui/tui.go
@@ -2,11 +2,10 @@ package ui
 
 import (
 	"log"
-	"strconv"
-	"strings"
 
+	"github.com/Ayushi40804/PoRtScAnNeR/internal/scanner" // Replace with your actual module path
+	"github.com/Ayushi40804/PoRtScAnNeR/internal/utils"   // Import utils package
 	"github.com/rivo/tview"
-	"github.com/yourname/portscanner/internal/scanner" // Import scanner package to perform scans
 )
 
 // ShowUI initializes and displays the terminal UI
@@ -19,29 +18,7 @@ func ShowUI(app *tview.Application) error {
 		AddDropDown("Scan Type", []string{"TCP", "UDP"}, 0, nil).
 		AddInputField("Ports", "80,443,8080", 20, nil, nil).
 		AddButton("Start Scan", func() {
-			// Clear previous results
-			resultsTextView.SetText("Results will appear here...\n")
-
-			// Get values from the form
-			hostname := form.GetFormItemByLabel("Hostname").(*tview.InputField).GetText()
-			_, scanType := form.GetFormItemByLabel("Scan Type").(*tview.DropDown).GetCurrentOption()
-			portsInput := form.GetFormItemByLabel("Ports").(*tview.InputField).GetText()
-
-			// Convert ports from string to slice of integers
-			ports := parsePorts(portsInput)
-
-			// Call scanner logic based on the selected scan type
-			log.Printf("Scanning %s with %s scan on ports %v", hostname, scanType, ports)
-			switch scanType {
-			case "TCP":
-				resultsTextView.SetText("Starting TCP Scan...\n")
-				scanner.TCPConnectScan(hostname, ports, resultsTextView)
-			case "UDP":
-				resultsTextView.SetText("Starting UDP Scan...\n")
-				scanner.UDPScan(hostname, ports, resultsTextView)
-			default:
-				log.Println("Invalid scan type selected")
-			}
+			startScan(form, resultsTextView)
 		}).
 		AddButton("Quit", func() {
 			app.Stop() // Exit the application
@@ -59,14 +36,33 @@ func ShowUI(app *tview.Application) error {
 	return app.SetRoot(layout, true).Run()
 }
 
-// parsePorts converts a comma-separated list of port numbers (as a string) into a slice of integers.
-func parsePorts(ports string) []int {
-	var result []int
-	for _, p := range strings.Split(ports, ",") {
-		port, err := strconv.Atoi(strings.TrimSpace(p))
-		if err == nil {
-			result = append(result, port)
-		}
+// startScan performs the actual scan based on user input
+func startScan(form *tview.Form, resultsTextView *tview.TextView) {
+	// Clear previous results
+	resultsTextView.SetText("Results will appear here...\n")
+
+	// Get values from the form
+	hostname := form.GetFormItemByLabel("Hostname").(*tview.InputField).GetText()
+	_, scanType := form.GetFormItemByLabel("Scan Type").(*tview.DropDown).GetCurrentOption()
+	portsInput := form.GetFormItemByLabel("Ports").(*tview.InputField).GetText()
+
+	// Convert ports from string to slice of integers using utils.ParsePorts
+	ports, err := utils.ParsePorts(portsInput)
+	if err != nil {
+		resultsTextView.SetText("Invalid port input: " + err.Error() + "\n")
+		return
 	}
-	return result
+
+	// Call scanner logic based on the selected scan type
+	log.Printf("Scanning %s with %s scan on ports %v", hostname, scanType, ports)
+	switch scanType {
+	case "TCP":
+		resultsTextView.SetText("Starting TCP Scan...\n")
+		scanner.TCPConnectScan(hostname, ports, resultsTextView)
+	case "UDP":
+		resultsTextView.SetText("Starting UDP Scan...\n")
+		scanner.UDPScan(hostname, ports, resultsTextView)
+	default:
+		log.Println("Invalid scan type selected")
+	}
 }
